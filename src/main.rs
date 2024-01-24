06e00de441967bc477006f4ec10578fa894badb0
andresbermeoq <andresbermeoq1@gmail.com>
2024-01-23 22:58:04 -0500

    feat: :sparkles: Add tokio select
    Add tokio select in the main and change the type in the channel

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,41 @@
+use std::net::SocketAddr;
+
 use tokio::{
     io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
     net::TcpListener,
+    sync::broadcast,
 };
 
 #[tokio::main]
 async fn main() {
     // Async Chat
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
+
+    let (sender, receiver) = broadcast::channel(10);
     loop {
-        let (mut socket, _addr) = listener.accept().await.unwrap();
+        let (mut socket, addr) = listener.accept().await.unwrap();
+        let sender = sender.clone();
+        let mut receiver = sender.subscribe();
 
         tokio::spawn(async move {
             let (reader, mut writer) = socket.split();
             let mut reader = BufReader::new(reader);
             let mut line = String::new();
+
             loop {
-                let bytes_read = reader.read_line(&mut line).await.unwrap();
-                if bytes_read == 0 {
-                    break;
+                tokio::select! {
+                    _ = reader.read_line(&mut line) => {
+                        sender.send((line.clone(), addr)).unwrap();
+                        line.clear();
+                    }
+                    result = receiver.recv() => {
+                        let (message, message_address) = result.unwrap();
+                        if addr != message_address {
+                            writer.write_all(message.as_bytes()).await.unwrap();
+                        }
+                    }
+
                 }
-                writer.write_all(line.as_bytes()).await.unwrap();
-                line.clear();
             }
         });
     }
